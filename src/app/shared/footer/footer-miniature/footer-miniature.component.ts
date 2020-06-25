cbc5c3c5afb6245cab627fefbaff2a078438ca41
Elisabeth Azémard <elie.azemard@gmail.com>
2020-06-26 01:42:40 +0200

    Last of the home structure & blocks contents. Missing styles.

--- src/app/shared/footer/footer-miniature/footer-miniature.component.ts
+++ src/app/shared/footer/footer-miniature/footer-miniature.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FooterMiniatureComponent implements OnInit {
 
-  @Input() image: string = "./assets/footer-miniature.png";
+  @Input() image: string = "./assets/footer/foot1.png";
   @Input() title: string = "Geo en version papier.";
   @Input() description: string = "Chaque semaine, recevez l'édition papier de Geo, livrée directement chez vous.";
   @Input() buttonName: string = "Je découvre";
